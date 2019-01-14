2564fb16e4bbcf0e4bcc13f1ab0d8a762fdc515e
Rajesh <rajesh.das85@gmail.com>
2019-01-14 17:13:21 +0530

    Commited No Review

--- _helpers/db.js
+++ _helpers/db.js
@@ -1,7 +1,7 @@
 const config = require('config.json');
 const database = require('../config/database'); 			// load the database config
 const mongoose = require('mongoose');
-mongoose.connect(database.remoteUrl,
+mongoose.connect(process.env.CUSTOMCONNSTR_MyConnString || database.remoteUrl,
 { 
     useCreateIndex: true,
     useNewUrlParser: true 
