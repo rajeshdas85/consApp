f2d2e3a3174ce6e22a3920529e39cbf31b73725b
Rajesh <rajesh.das85@gmail.com>
2019-01-23 18:29:55 +0530

    No Review

--- _helpers/db.js
+++ _helpers/db.js
@@ -1,7 +1,7 @@
 const config = require('config.json');
 const database = require('../config/database'); 			// load the database config
 const mongoose = require('mongoose');
-mongoose.connect(database.localUrl,
+mongoose.connect(database.awsAdminUrl,
 { 
     useCreateIndex: true,
     useNewUrlParser: true 
